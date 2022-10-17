5c88424feb75e57b3adf2a5fdfe9ffbdbd13b711
ouafa benamor <ouafa.benamor@gmail.com>
2022-10-17 04:49:27 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "./Compenent/Main";
+import "./Compenent/Style.css";
+import Card from "./Compenent/Card";
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
+   
